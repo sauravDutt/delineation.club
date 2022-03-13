414a5cd4a91ceb8c33b5abd96affe94f3c03c6ae
sauravDutt <sauravd069@gmail.com>
2022-03-13 14:02:42 +0530

    Just added some more stuff

--- main.js
+++ main.js
@@ -16,3 +16,19 @@ renderer.setSize ( window.innerWidth, window.innerHeight );
 camera.position.setZ(30);
 
 renderer.render( scene, camera );
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true} );
+const torus = new THREE.Mesh( geometry, material );
+
+scene.add(torus)
+
+function animate() {
+  requestAnimationFrame( animate );
+
+  torus.rotation.x += 0.01;
+
+  renderer.render( scene, camera);
+}
+
+animate()
